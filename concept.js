6e7f540ae009d129478a1969531af77b59c2acff
tahid02 <tanzim1463@gmail.com>
2021-04-10 09:47:54 +0600

    basic crud operation of node and mongodb

---
+++ concept.js
@@ -0,0 +1,6 @@
+// console.log(global);
+// console.log(module);
+// const os = require('os')
+// console.log(os);
+const express = require('express')
+console.log(express)
---
+++ index.js
@@ -0,0 +1,92 @@
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectID
+// const password  = "2b_$mSXgdsx#6Ef"; this password throw errors
+const password = "user"; // now error removes
+
+
+const uri = "mongodb+srv://organicUser:user@cluster0.z6ers.mongodb.net/organicdb?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+app.get('/', (req, res) => {
+    // res.send({name:'kamal'})
+    res.sendFile(__dirname + '/index.html')
+})
+
+
+// const MongoClient = require('mongodb').MongoClient;
+
+client.connect(err => {
+    const productCollection = client.db("organicdb").collection("products");
+
+    app.get('/products', (req,res) =>{
+        productCollection.find({})
+        .toArray( (err, documents) => {
+            res.send(documents)
+        })
+    })
+
+    app.post("/addProduct", (req, res) => {
+        const product = req.body;
+        // console.log(product);
+        productCollection.insertOne(product)
+        .then( result => {
+            console.log('data added successfully');
+            res.send(result)
+            res.redirect('/')
+        })
+    
+        
+    })
+
+
+    app.get('/product/:id', (req,res) => {
+        console.log(req.params.id);
+
+        productCollection.find({ _id: ObjectId(req.params.id)})     
+        .toArray( (err, documents) => {
+            res.send(documents[0]);
+        })
+       
+    })
+
+    app.patch('/update/:id' , (req, res) => {
+        // console.log(req.body.price);
+
+        productCollection.updateOne(
+            {_id: ObjectId(req.params.id)},
+            {
+                $set:{price: req.body.price, quantity: req.body.quantity}
+
+            }
+            )
+            .then( result => {
+                // console.log(result);
+                res.send(result.modifiedCount > 0)
+            })
+        
+    })
+
+    app.delete('/delete/:id', (req,res) =>{
+        console.log(req.params.id);
+            // perform actions on the collection object
+
+        productCollection.deleteOne({_id: ObjectId(req.params.id)})
+        .then( (result) =>{
+            // console.log(result);
+            res.send(result.deletedCount > 0)
+        })
+    })
+
+    console.log('database connected');
+    //   client.close();
+});
+
+
+app.listen(3000, () => console.log("listening to localhost 3000"))
